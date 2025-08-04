118a48b83a9ff5213ba2e598da588495802a3957
Sadam Root <86251542+rootprotest@users.noreply.github.com>
2025-08-04 13:11:17 +0530

    last change in code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,27 @@ import LatestArticles from "@/components/Home/LatestArticles";
 import SpecialitiesSection from "@/components/Home/SpecialitiesSection";
 import TestimonialSection from "@/components/Home/TestimonialSection";
 
+
 export const metadata: Metadata = {
-  title: "Dr Shabeer Ahmed | Gastrointestinal & Laparoscopic Surgeon",
+ title: "Dr Shabeer Ahmed | Gastrointestinal & Laparoscopic Surgeon",
   description: "Dr. Shabeer Ahmed is a renowned Gastrointestinal and Laparoscopic Surgeon, specializing in advanced minimally invasive procedures with exceptional patient care.",
   keywords: "Dr Shabeer Ahmed, Gastrointestinal Surgeon, Laparoscopic Surgeon, Colorectal Surgeon, Metabolic Surgery",
+  alternates: {
+    canonical: 'https://www.drshabeerahmed.in/',
+  },
+  openGraph: {
+    title: 'Dr. Shabeer Ahmed is a renowned Gastrointestinal and Laparoscopic Surgeon, specializing in advanced minimally invasive procedures with exceptional patient care.',
+    description:
+      'Consult Dr. Shabeer Ahmed is a renowned Gastrointestinal and Laparoscopic Surgeon, specializing in advanced minimally invasive procedures with exceptional patient care.',
+    url: 'https://www.drshabeerahmed.in/',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
+
 
 export default function Home() {
   return (
